536e892f16f76c0459f7139d0be8010d0bca86df
Juanjo Siles <jjsiles98@gmail.com>
2023-02-18 00:48:52 +0100

    styling

--- app/frontend/jquery.js
+++ app/frontend/jquery.js
@@ -1,3 +1,22 @@
-function loadAdvancedSearch() {
-	$('#advancedSearch').load("./frontend/static/pages/advancedSearch.html");
+function loadStart() {
+	document.getElementById('content-section').style.display = 'none';
+	document.getElementById('start-section').style.display = 'block';
+}
+
+function loadAdvancedSimulation() {
+	document.getElementById('start-section').style.display = 'none';
+	$('#content-section').load("./frontend/static/pages/advancedSimulation.html");
+	document.getElementById('content-section').style.display = 'block';
+}
+
+function loadSimpleSimulation() {
+	document.getElementById('start-section').style.display = 'none';
+	$('#content-section').load("./frontend/static/pages/simpleSimulation.html");
+	document.getElementById('content-section').style.display = 'block';
+}
+
+function loadPostProcess() {
+	document.getElementById('start-section').style.display = 'none';
+	$('#content-section').load("./frontend/static/pages/postprocess.html");
+	document.getElementById('content-section').style.display = 'block';
 }
